4bd7f6935f7ee662ff86db071b1b4d38349cf2cc
Conor Briggs <91425364+Miridicent@users.noreply.github.com>
2024-02-27 12:30:51 +0000

    Update Movement_2Way.cs

--- Dissertation Artifiact/Assets/Scripts/Movement_2Way.cs
+++ Dissertation Artifiact/Assets/Scripts/Movement_2Way.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 
 public class Movement_2Way : MonoBehaviour
 {
+    //Speed and jump height 
     public float Speed = 2;
     public float Jump = 1;
+
+    // Calls the Rigid body and the Sprite renderer 
     private Rigidbody2D _rb;
     private SpriteRenderer _sprite;
     public bool flipX;
@@ -20,14 +23,17 @@ public class Movement_2Way : MonoBehaviour
 
     private void Update()
     {
+        //Sets the controls so that if A and D are pressed the player can move left and right (also works for arrows)
         var movement = Input.GetAxis("Horizontal");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * Speed;
 
+        //Allows the player to jump
         if(Input.GetButtonDown("Jump") && Mathf.Abs(_rb.velocity.y) < 0.001f)
         {
             _rb.AddForce(new Vector2(0, Jump), ForceMode2D.Impulse);
         }
 
+        //Flips the sprite depending on the key pressed
         if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyUp(KeyCode.A) )
         {
             _sprite.flipX = true;
