289564b483ea9a384f426c2aab346808c0543cb0
Conor Briggs <91425364+Miridicent@users.noreply.github.com>
2024-02-27 12:43:47 +0000

    Update Drag_and_Drop.cs

--- Dissertation Artifiact/Assets/Scripts/Drag_and_Drop.cs
+++ Dissertation Artifiact/Assets/Scripts/Drag_and_Drop.cs
@@ -8,6 +8,7 @@ public class Drag_and_Drop : MonoBehaviour
     //Vector3 mousePosition;
     // Start is called before the first frame update
 
+    // the game object clicked on will equal selectedObject 
     public GameObject selectedObject;
     Vector3 offset;
     void Start()
@@ -18,9 +19,10 @@ public class Drag_and_Drop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Gets mouse position
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-
+        // if the left mouse button is down and the mouse overlaps with a trigger, drag the game object 
         if (Input.GetMouseButtonDown(0))
         {
             Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
@@ -30,12 +32,12 @@ public class Drag_and_Drop : MonoBehaviour
                 offset = selectedObject.transform.position - mousePosition;
             }
         }
-
+        // drops object 
         if (Input.GetMouseButtonUp(0) && selectedObject)
         {
             selectedObject = null;
         }
-
+        // places object where the mouse lets go of it
         if (selectedObject)
         {
             selectedObject.transform.position = mousePosition + offset;
