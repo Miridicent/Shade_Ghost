a46daa156d4bf58d7831864f924d67b3ca86b892
Conor Briggs <91425364+Miridicent@users.noreply.github.com>
2024-03-12 11:21:53 +0000

    Misc

---
+++ Dissertation Artifiact/Assets/Scripts/Drag_and_Drop_key.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drag_and_Drop_key : MonoBehaviour
+{
+
+    //Vector3 mousePosition;
+    // Start is called before the first frame update
+
+    // the game object clicked on will equal selectedObject 
+    public GameObject selectedObject;
+    Vector3 offset;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gets mouse position
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // if the left mouse button is down and the mouse overlaps with a trigger, drag the game object 
+        if (Input.GetMouseButtonDown(0))
+        {
+            Collider2D targetObject = Physics2D.OverlapPoint(mousePosition);
+            if (targetObject)
+            {
+                selectedObject = targetObject.transform.gameObject;
+                offset = selectedObject.transform.position - mousePosition;
+            }
+        }
+        // drops object 
+        if (Input.GetMouseButtonUp(0) && selectedObject)
+        {
+            selectedObject = null;
+        }
+        // places object where the mouse lets go of it
+        if (selectedObject)
+        {
+            selectedObject.transform.position = mousePosition + offset;
+        }
+    }
+}
--- Dissertation Artifiact/Assets/Scripts/Puzzle_Start.cs
+++ Dissertation Artifiact/Assets/Scripts/Puzzle_Start.cs
@@ -3,23 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+
 public class Puzzle_Start : MonoBehaviour
 {
+
+    public string sceneManager;
+    private bool isInRange = false;
     public string targetTag = "Player";
     // Start is called before the first frame update
     private void OnTriggerStay2D(Collider2D collider)
     {
-        if(collider.CompareTag(targetTag) && Input.GetKeyDown(KeyCode.Space))
+
+        if (collider.CompareTag("Player")) // Assuming the character has a "Player" tag
         {
-            Debug.Log("Player entered the gate trigger");
-            SceneManager.LoadScene("Gate_Puzzle");
+            isInRange = true; // Set the flag to true when the character enters the trigger zone
         }
-        
+
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerExit2D(Collider2D other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            isInRange = false; // Set the flag to false when the character exits the trigger zone
+        }
+    }
+
+    
+
+// Update is called once per frame
+void Update()
+    {
+        if (isInRange && Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Player entered the gate trigger");
+            SceneManager.LoadScene(sceneManager);
+        }
+
     }
 }
