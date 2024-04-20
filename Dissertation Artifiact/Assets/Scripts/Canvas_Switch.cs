25b231fab23af5e09b09a296f34ace32e5d57b80
Conor Briggs <91425364+Miridicent@users.noreply.github.com>
2024-04-20 21:32:55 +0100

    Update Canvas_Switch.cs

--- Dissertation Artifiact/Assets/Scripts/Canvas_Switch.cs
+++ Dissertation Artifiact/Assets/Scripts/Canvas_Switch.cs
@@ -7,6 +7,7 @@ public class Canvas_Switch : MonoBehaviour
     public GameObject Canva;
     public GameObject Canva2;
 
+// when the function is run, switch which canvases are active 
     public void SwitchCanavs()
     {
         Canva.SetActive(false);
