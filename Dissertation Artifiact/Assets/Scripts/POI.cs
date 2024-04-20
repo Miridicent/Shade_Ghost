8190375e42a5a1ea2f744dfd3515e1a635f51b64
Conor Briggs <91425364+Miridicent@users.noreply.github.com>
2024-04-20 21:03:08 +0100

    Update POI.cs

--- Dissertation Artifiact/Assets/Scripts/POI.cs
+++ Dissertation Artifiact/Assets/Scripts/POI.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class POI : MonoBehaviour
 {
+    //Gets the Poi game object and the shine audio clip
     new public GameObject gameObject;
     public AudioClip clip;
     public AudioSource source;
@@ -14,12 +15,14 @@ public class POI : MonoBehaviour
         clip = GetComponent<AudioClip>();   
     }
 
+    // when the player is in range of something they can inspect then the mark appears and players the audio clip
     private void OnTriggerEnter2D(Collider2D other)
     {
         gameObject.SetActive(true); 
         source.Play();
     }
 
+    // when no longer in range the mark will become inactive
     private void OnTriggerExit2D(Collider2D other)
     {
         gameObject.SetActive(false);
