d4622d0c273f4d0c006bb06a8f05cd97c6fa996a
Conor Briggs <91425364+Miridicent@users.noreply.github.com>
2024-04-20 21:21:59 +0100

    Update Pendent_holder.cs

--- Dissertation Artifiact/Assets/Scripts/Pendent_holder.cs
+++ Dissertation Artifiact/Assets/Scripts/Pendent_holder.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//keeps track of how many pendents the player has, this is used in the final gate script
 public class Pendent_holder : MonoBehaviour
 {
     public static int Pendent = 0;
