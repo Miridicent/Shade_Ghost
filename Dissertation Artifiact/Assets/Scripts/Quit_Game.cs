1de6699d34352d11fe34fd27911db13c266ec2a3
Conor Briggs <91425364+Miridicent@users.noreply.github.com>
2024-04-20 21:14:54 +0100

    Update Quit_Game.cs

--- Dissertation Artifiact/Assets/Scripts/Quit_Game.cs
+++ Dissertation Artifiact/Assets/Scripts/Quit_Game.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Quits the game upon pressing the quit button
 public class Quit_Game : MonoBehaviour
 {
     public void Quit()
