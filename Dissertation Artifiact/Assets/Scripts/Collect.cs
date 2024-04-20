74917d420fa11c499a7bb4140cc1c8b27b744f14
Conor Briggs <91425364+Miridicent@users.noreply.github.com>
2024-04-20 21:09:09 +0100

    Update Collect.cs

--- Dissertation Artifiact/Assets/Scripts/Collect.cs
+++ Dissertation Artifiact/Assets/Scripts/Collect.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Collect : MonoBehaviour
 {
 
+// Creates the variables 
     bool IsinRange;
     public int unlock = 0;
     GameObject collidedObject;
@@ -13,6 +14,7 @@ public class Collect : MonoBehaviour
     public AudioClip clip;
     public AudioSource source;
 
+    // When colliding with an onject the object being collided with is assinged to the collision game object 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Key_Frag") || collision.CompareTag("Gate"))
@@ -39,15 +41,17 @@ public class Collect : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Check if in range and player presses the "Return" key
+        // Check if in range and player presses the "Enter" key
         if (IsinRange && collidedObject.gameObject.CompareTag("Key_Frag") && Input.GetKeyDown(KeyCode.Return))
         {
+        // Unlock is incremented by 1
             unlock++;
             // Deactivate the collected fragment
             collidedObject.gameObject.SetActive(false);
             source.Play();
         }
 
+        // when all keys are collected and the gate is in range then the gate will open
         if (unlock == 3 && collidedObject.gameObject.CompareTag("Gate") && IsinRange)
         {
             Gate.SetActive(false);
